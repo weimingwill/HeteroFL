88bcb42a66891c395dfaeed78e595c08b639c237
Weiming <wingalong@gmail.com>
2021-07-12 18:14:11 +0800

    fix script

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,6 +14,6 @@ fi
 
 srun -u --partition=innova --job-name=${fraction}-heterofl \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
-    python ${project_dir}/src/train_classifier_fed.py/fedreid/main.py \
+    python ${project_dir}/src/train_classifier_fed.py \
       --data_name CIFAR10 --model_name resnet18 \
       --control_name 1_100_${fraction}_non-iid-2_dynamic_a1-b1-c1-d1-e1_gn_0_0 2>&1 | tee log/users_${fraction}-${now}.log &
