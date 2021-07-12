cdf6485adb52c5b6ff63b2023652077d390aec4e
Weiming <wingalong@gmail.com>
2021-07-12 18:23:07 +0800

    update scripts

--- scripts/run_mnist.sh
+++ scripts/run_mnist.sh
@@ -6,14 +6,14 @@ project_dir=$root_dir/playground/HeteroFL
 
 export PYTHONPATH=$PYTHONPATH:${project_dir}
 
-fraction=$1 #
+fraction=$1
 if [ -z "$1" ]
   then
     fraction=0.1
 fi
 
-srun -u --partition=innova --job-name=${fraction}-heterofl \
+srun -u --partition=innova --job-name=${fraction}-mnist-heterofl \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
     python ${project_dir}/src/train_classifier_fed.py \
-      --data_name CIFAR10 --model_name resnet18 \
-      --control_name 1_100_${fraction}_non-iid-2_dynamic_a1-b1-c1-d1-e1_gn_0_0 2>&1 | tee log/users_${fraction}-${now}.log &
+      --data_name MNIST --model_name conv \
+      --control_name 1_100_${fraction}_non-iid-5_dynamic_a1-b1-c1-d1-e1_bn_1_1 2>&1 | tee log/users_mnist_${fraction}-${now}.log &
