92b68176a17bbc94bc313ef3ddb2d816500f517f
Weiming <wingalong@gmail.com>
2021-07-13 00:53:56 +0800

    add script

---
+++ scripts/run_wiki.sh
@@ -0,0 +1,19 @@
+mkdir -p log
+now=$(date +"%Y%m%d_%H%M")
+
+root_dir=/mnt/lustre/$(whoami)
+project_dir=$root_dir/playground/HeteroFL
+
+export PYTHONPATH=$PYTHONPATH:${project_dir}
+
+fraction=$1
+if [ -z "$1" ]
+  then
+    fraction=0.1
+fi
+
+srun -u --partition=innova --job-name=${fraction}-wiki-heterofl \
+    -n1 --gres=gpu:1 --ntasks-per-node=1 \
+    python ${project_dir}/src/test_transformer_fed.py \
+      --data_name WikiText2 --model_name transformer \
+      --control_name 1_100_${fraction}_non-iid_fix_a2-b2-c2-d2-e2_none_1_0 2>&1 | tee log/wiki_${fraction}-${now}.log &
