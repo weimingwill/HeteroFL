bc6711a4c356dcf4e6cb2fe36b54ae02fff4c003
Weiming <wingalong@gmail.com>
2021-07-15 22:58:10 +0800

    add script

---
+++ scripts/run_test_mnist_iid_fix.sh
@@ -0,0 +1,27 @@
+mkdir -p log
+now=$(date +"%Y%m%d_%H%M")
+
+root_dir=/mnt/lustre/$(whoami)
+project_dir=$root_dir/playground/HeteroFL
+
+export PYTHONPATH=$PYTHONPATH:${project_dir}
+
+fraction=$1
+if [ -z "$1" ]
+  then
+    fraction=0.1
+fi
+
+fix=$2
+if [ -z "$2" ]
+  then
+    fix=a5-e5
+fi
+
+
+srun -u --partition=innova --job-name=${fraction}-${fix}-mnist-heterofl \
+    -n1 --gres=gpu:1 --ntasks-per-node=1 \
+    python ${project_dir}/src/test_classifier_fed.py \
+      --data_name MNIST --model_name conv \
+      --control_name 1_100_${fraction}_iid_fix_${fix}_bn_1_1 2>&1 | tee log/users_mnist_test_${fix}_${fraction}-${now}.log &
+
